5c2242efeb5b9e8df9ba2943e12021846fc14422
AlexanderMatveev2908 <matveevalexander470@gmail.com>
2025-11-04 16:03:59 +0200

    🪾 header => 🛠️ built search input smaller screens

--- apps/client/src/layout/header/header.ts
+++ apps/client/src/layout/header/header.ts
@@ -1,15 +1,19 @@
 import { SvgFillHalfSun } from '@/common/components/svgs/fill/half_sun/half-sun';
 import { SvgT } from '@/common/types/etc';
 import { NgComponentOutlet } from '@angular/common';
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, computed, Signal } from '@angular/core';
+import { FormFieldTxt } from '@/common/components/forms/base_fields/form_field_txt/form-field-txt';
+import { SvgFillSearch } from '@/common/components/svgs/fill/search/search';
 
 @Component({
   selector: 'app-header',
-  imports: [NgComponentOutlet],
+  imports: [NgComponentOutlet, FormFieldTxt],
   templateUrl: './header.html',
   styleUrl: './header.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class Header {
   public readonly logo: SvgT = SvgFillHalfSun;
+
+  public readonly searchSvg: Signal<SvgT> = computed(() => SvgFillSearch);
 }
