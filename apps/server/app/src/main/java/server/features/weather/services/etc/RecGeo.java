394d83bb6d4c4e8f1b8d7c02d1beea77fead0029
AlexanderMatveev2908 <matveevalexander470@gmail.com>
2025-11-05 23:50:51 +0200

    🪾 main => ⚠️ _

--- apps/server/app/src/main/java/server/features/weather/services/etc/RecGeo.java
+++ apps/server/app/src/main/java/server/features/weather/services/etc/RecGeo.java
@@ -2,14 +2,23 @@ package server.features.weather.services.etc;
 
 import java.util.Map;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
 import server.decorators.flow.ErrAPI;
 import server.lib.data_structure.prs.LibPrs;
 
+@JsonIgnoreProperties(ignoreUnknown = true)
 public record RecGeo(double lat, double lon, String country_code, String region) {
 
   public static RecGeo fromIpApiBody(Map<String, Object> map) {
     try {
-      return LibPrs.tFromMap(map, RecGeo.class);
+      return new RecGeo(
+          (double) map.get("latitude"),
+          (double) map.get("longitude"),
+          (String) map.get("country_code"),
+          (String) map.get("region")
+
+      );
     } catch (Exception err) {
       throw new ErrAPI("invalid arg geolocation");
     }
@@ -20,6 +29,14 @@ public record RecGeo(double lat, double lon, String country_code, String region)
 
     map.put("strategy", "spring");
 
+    // Map<String, Object> testMap = Map.of(
+    //     "region", "Bacău",
+    //     "country_code", "RO",
+    //     "lon", 26.5252663,
+    //     "lat", 46.3227827,
+    //     "strategy", "spring");
+
+    // return testMap;
     return map;
   }
 }
